7d27e1df768612f57b46691a17fd4e184ef6d258
caralocke <cara.m.locke@gmail.com>
2021-07-06 17:39:47 -0400

    Added some styling

--- github-usercard/src/components/User.js
+++ github-usercard/src/components/User.js
@@ -3,7 +3,7 @@ import React from 'react'
 class User extends React.Component {
 render(){
     return(
-        <div>
+        <div className='user'>
             <img src={this.props.user.avatar_url} alt='avatar'/>
             <h3>Name: {this.props.user.name}</h3>
             <h4>Username: {this.props.user.login}</h4>
